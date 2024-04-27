d2075dddebb027babb2771f0c40c3cefdbd690aa
Joseph Wambua <101257432+josefwambua@users.noreply.github.com>
2024-04-27 10:21:41 +0000

    Initial commit

---
+++ django_project/views.py
@@ -0,0 +1,27 @@
+import requests
+from django.shortcuts import render
+
+
+def home(request):
+  # Fetching movie data from the TMDB API
+  APILINK = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=1a6c1a09a92f40dfa8f3c6dc254af26b&page=1'
+  response = requests.get(APILINK)
+  data = response.json()
+  movies = data.get('results', [])
+
+  return render(request, 'templates/index.html', {'movies': movies})
+
+
+def about(request):
+  # Logic to fetch data or perform any other operations
+  return render(request, 'about.html')
+
+
+def recently_watched(request):
+  # Logic to fetch data or perform any other operations
+  return render(request, 'recently_watched.html')
+
+
+def wishlist(request):
+  # Logic to fetch data or perform any other operations
+  return render(request, 'wishlist.html')
